23a7959fff3b844811e20ddcb9207ba66c65f17d
Алексей Зендель <alexeizendel@yandex.ru>
2025-02-09 17:15:08 +0500

    main.py теперь также запускает botsite/server.go

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import bot
+import threading
+import os
 
 
 if __name__ == '__main__':
+    def start_site():
+        os.system("go run botsite/server.go")
+    threading.Thread(target=start_site).start()
+
     bot.run_discord_bot()
